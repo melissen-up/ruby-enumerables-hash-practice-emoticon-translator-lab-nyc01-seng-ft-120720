d4f6356eb05e38ae09786be526cd95f2a9cb7149
Melissa F <melissa.faulner@gmail.com>
2020-12-03 19:48:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,11 +16,11 @@ def load_library(file_path)
   # binding.pry
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
 end
 
-def get_japanese_emoticon(file_path, emoticon)
+def get_japanese_emoticon
   
 end
 
