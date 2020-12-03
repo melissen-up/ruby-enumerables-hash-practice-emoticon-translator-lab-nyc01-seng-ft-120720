d061a3f9f9c5ec2d3483f4373d8fed71bad17a10
Melissa F <melissa.faulner@gmail.com>
2020-12-03 19:42:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,14 +3,14 @@ require "pry"
 
 def load_library(file_path)
   library = YAML.load_file(file_path)
+  new_hash = {}
   
-  new_hash = { }
   library.each do |key, value|
     # binding.pry
-    new_hash[key] = { }
+    new_hash[key] = {}
     new_hash[key][:english] = value[0]
     new_hash[key][:jampanese] = value[1]
-    binding.pry
+    # binding.pry
   end
   library
 end
