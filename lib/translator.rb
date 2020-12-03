621af82cb6dde45e818e22bc0d7c501f12252e2c
Melissa F <melissa.faulner@gmail.com>
2020-12-03 19:33:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 require "yaml"
 require "pry"
 
-def load_library(yaml_file)
-  library = YAML.load_file(yaml_file)
+def load_library(path)
+  library = YAML.load_file(path)
   
   new_hash = {}
   library.each do |key, value|
