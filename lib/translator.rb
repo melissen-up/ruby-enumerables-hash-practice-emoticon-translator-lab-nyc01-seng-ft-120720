23609ddf491671f29d926a422b68028da11b5bab
Melissa F <melissa.faulner@gmail.com>
2020-12-03 19:47:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,11 +15,10 @@ def load_library(file_path)
   emoticon_hash
   # binding.pry
 end
-
+def get_english_meaning
+  # code goes here
+end
 def get_japanese_emoticon(file_path, emoticon)
   
 end
 
-def get_english_meaning
-  # code goes here
-end
